53dbe4c802478cc0d97f35839824bd2f898e8823
Protypes <protypes773@gmail.com>
2024-03-12 22:11:13 +0200

    1.0

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Initialize Swiper
-  var swiper = new Swiper('.hero-carousel', {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
     direction: 'horizontal',
     loop: true,
-    autoplay: 1000,
-    autoplayStopOnLast: false, // loop false also
+    speed: 1000,
+    // If we need pagination
     pagination: {
-      el: '.hero-carousel__pagination',
-      clickable: true,
+      el: '.swiper-pagination',
+      clickable: true
     },
-    // Specify your custom slide class
-    slideClass: 'hero-carousel__container__item',
+    autoplay: {
+      delay: 3000
+    },
   });
 });
